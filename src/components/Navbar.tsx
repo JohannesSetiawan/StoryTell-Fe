93f1710a45f1c85ad44c9714d5e291307fa0c313
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2024-06-17 10:09:21 +0700

    [Fix] Fix navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,7 +33,7 @@ export function Navbar() {
   if (!user_token) {
     return (
       <div className="flex w-full justify-between gap-4 px-4 py-2 md:px-8">
-        <div className="text-xl font-bold">Welcome to Gladi!</div>
+        <div className="text-xl font-bold">Welcome to Storytell!</div>
         <div className="flex flex-row gap-6 items-center">
           <ToggleTheme />
           <button
