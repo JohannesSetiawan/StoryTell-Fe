96be9112dc3845fc9cd384f3f4c88ab6637c1f77
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2024-06-18 11:14:15 +0700

    [Feat] Add search bar in story list.

--- src/modules/StoryListPage.tsx
+++ src/modules/StoryListPage.tsx
@@ -1,10 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import { useGetAllStoriesQuery } from "../redux/api/storyApi";
+import { useState } from "react";
 
 export function StoryListPage() {
   const navigate = useNavigate();
 
   const {data: stories} = useGetAllStoriesQuery()
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredStories = stories?.filter(story =>
+    story.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
   
   const handleBack = () => {
     navigate("/");
@@ -18,13 +28,19 @@ export function StoryListPage() {
             All Stories
           </h2>
           <div className="flex flex-wrap gap-3 w-full py-5">
-          
+            <input
+                type="text"
+                value={searchQuery}
+                onChange={handleSearchChange}
+                placeholder="Search by title..."
+                className="w-full px-3 py-2 placeholder-gray-400 text-gray-700 bg-gray rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
           </div>
 
           <div className="flex flex-wrap gap-3 w-full py-5 px-4">
             <div className="w-full">
               {
-                stories?.map((story) => (
+                filteredStories?.map((story) => (
                   <a href={`/read-story/${story.id}`} className="block dark:text-indigo-300" key={story.id}>
                     <div className="w-full flex flex-col  justify-center bg-gray-200 dark:bg-gray-600 border-2 border-gray-400 dark:border-gray-500 p-4 rounded-lg shadow-md mb-4">
                       <p>Title: {story.title}</p>
--- src/modules/UserStoryListPage.tsx
+++ src/modules/UserStoryListPage.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useGetSpecificUserStoryQuery } from "../redux/api/storyApi";
 import { RootState, useAppSelector } from "../redux/store";
-
+import { useState } from "react";
 
 export function UserStoryListPage() {
   const navigate = useNavigate();
@@ -9,6 +9,15 @@ export function UserStoryListPage() {
   const userId = useAppSelector((state: RootState) => state.user).user?.userId;
   
   const {data: stories} = useGetSpecificUserStoryQuery(userId == undefined? "idSalah" : userId)
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredStories = stories?.filter(story =>
+    story.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
   
   const handleBack = () => {
     navigate("/");
@@ -59,10 +68,20 @@ export function UserStoryListPage() {
             </button>
           </div>
 
+          <div className="flex flex-wrap gap-3 w-full py-5">
+            <input
+                type="text"
+                value={searchQuery}
+                onChange={handleSearchChange}
+                placeholder="Search by title..."
+                className="w-full px-3 py-2 placeholder-gray-400 text-gray-700 bg-gray rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+          </div>
+
           <div className="flex flex-wrap gap-3 w-full py-5 px-4">
             <div className="w-full">
               {
-                stories?.map((story) => (
+                filteredStories?.map((story) => (
                   <a href={`/read-story/${story.id}`} className="block dark:text-indigo-300" key={story.id}>
                     <div className="w-full flex flex-col  justify-center bg-gray-200 dark:bg-gray-600 border-2 border-gray-400 dark:border-gray-500 p-4 rounded-lg shadow-md mb-4">
                       <p>Title: {story.title}</p>
