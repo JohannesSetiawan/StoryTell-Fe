faa20d9a4189d5bb4a3aa7fc0155eed5f51813a8
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2024-06-18 11:16:47 +0700

    [Feat] Add back button in read story page.

--- src/modules/ReadStoryPage.tsx
+++ src/modules/ReadStoryPage.tsx
@@ -95,22 +95,28 @@ export function ReadStoryPage() {
         }
         return (
             <div className="p-4 max-w-3xl mx-auto">
-            <h1 className="text-3xl font-bold mb-4">{story.title}</h1>
-            <p className="text-lg mb-6 whitespace-pre-line">{story.description}</p>
-            <h2 className="text-2xl font-semibold mb-3">Chapters</h2>
-            <div className="flex flex-wrap gap-3 w-full py-5 px-4">
-            <div className="w-full h-80 overflow-y-auto">
-              {
-                story.chapters?.map((chapter) => (
-                  <a href={`/read-chapter/${chapter.id}`} className="block dark:text-indigo-300" key={chapter.id}>
-                    <div className="w-full flex flex-col  justify-center bg-gray-200 dark:bg-gray-600 border-2 border-gray-400 dark:border-gray-500 p-4 rounded-lg shadow-md mb-4">
-                      <p>Chapter {chapter.order}. {chapter.title}</p>
-                    </div>
-                  </a>
-                ))
-              }
-            </div>
-          </div>
+              <h1 className="text-3xl font-bold mb-4">{story.title}</h1>
+              <p className="text-lg mb-6 whitespace-pre-line">{story.description}</p>
+              <h2 className="text-2xl font-semibold mb-3">Chapters</h2>
+              <div className="flex flex-wrap gap-3 w-full py-5 px-4">
+                <div className="w-full h-80 overflow-y-auto">
+                  {
+                    story.chapters?.map((chapter) => (
+                      <a href={`/read-chapter/${chapter.id}`} className="block dark:text-indigo-300" key={chapter.id}>
+                        <div className="w-full flex flex-col  justify-center bg-gray-200 dark:bg-gray-600 border-2 border-gray-400 dark:border-gray-500 p-4 rounded-lg shadow-md mb-4">
+                          <p>Chapter {chapter.order}. {chapter.title}</p>
+                        </div>
+                      </a>
+                    ))
+                  }
+                </div>
+              </div>
+              <button
+                    onClick={handleBack}
+                    className="rounded-lg flex flex-row items-center gap-2 justify-center bg-blue-500 text-white hover:bg-blue-600 dark:bg-gray-500 hover:dark:bg-gray-600 duration-200 transition-all ease-in-out px-4 py-2"
+                    >
+                    Back
+              </button>
             </div>
         );
     } else {
